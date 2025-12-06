68de1f505958f761e5d71183b9ece3c747debfe7
Ayşıl Simge Karacan <github@aysilsimge.dev>
2025-12-06 15:36:05 +0300

    create project

---
+++ SwiftAsyncCombineDemo/SwiftAsyncCombineDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftAsyncCombineDemo
+//
+//  Created by Ayşıl Simge Karacan on 6.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftAsyncCombineDemo/SwiftAsyncCombineDemo/SwiftAsyncCombineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAsyncCombineDemoApp.swift
+//  SwiftAsyncCombineDemo
+//
+//  Created by Ayşıl Simge Karacan on 6.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAsyncCombineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
